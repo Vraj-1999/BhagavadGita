6a0e1fceeb2814c16d1b85c47da4b7b0d9df6204
Vraj Patel <109271086+Vraj-1999@users.noreply.github.com>
2025-01-06 01:40:04 -1100

    files changes

---
+++ model/signup.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const logSchema = new mongoose.Schema({
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],  // Limited to these values
+      required: true
+    },
+    user_name:{
+        type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: { type: String, default: 'default.jpg' }
+    ,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    emailEnabled: { type: Boolean, default: true }
+});
+
+
+logSchema.pre("save", async function(next){
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password,salt)
+  next();
+})
+
+
+const signs = mongoose.model("signs", logSchema);
+
+module.exports = signs;
+  
